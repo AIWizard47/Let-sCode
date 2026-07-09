6af44e39deec3d7574622db953eb35c1c81e5cf6
Sam <sambhavvinayak16@gmail.com>
2026-07-10 00:02:12 +0530

    scripts changes

--- start_services.sh
+++ start_services.sh
@@ -5,6 +5,8 @@ echo "Starting services..."
 cd /home/kali/Desktop/Let-sCode
 source .venv/bin/activate
 
+cd /home/kali/Desktop/Let-sCode/CodePlatform/Code
+
 nohup python manage.py runserver 0.0.0.0:8000 > logs/django.log 2>&1 &
 
 nohup redis-server > logs/redis.log 2>&1 &
@@ -13,8 +15,10 @@ nohup celery -A CodePlatform worker --pool=solo -l info > logs/celery_worker.log
 
 nohup celery -A CodePlatform beat -l info > logs/celery_beat.log 2>&1 &
 
-cd /home/kali/Desktop/Let-sCode/Sandbox
+cd /home/kali/Desktop/Let-sCode/
 source .venv/bin/activate
+
+cd /home/kali/Desktop/Let-sCode/sandbox/sandbox/
 
 nohup python manage.py runserver 0.0.0.0:8080 > logs/sandbox.log 2>&1 &
 
